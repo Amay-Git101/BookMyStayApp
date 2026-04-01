089cbd007ed07a65159442370fe5517a0ddca40a
AmayGit-101 <amayjariwala@gmail.com>
2026-04-01 09:56:45 +0530

    UC4 - Room Search & Availability Check-changed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class UseCase4HotelBookingApp {
+public class Main {
 
     // ── RoomType enum ──────────────────────────────
     enum RoomType {
